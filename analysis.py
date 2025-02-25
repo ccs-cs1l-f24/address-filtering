36a4086c105bdf813f1db970f55775ce4d5ef3ff
zanielsen162 <zoeynielse162@gmail.com>
2025-02-25 12:22:45 -0800

    sad

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ import numpy.linalg as npla
 import os
 import pandas as pd
 from diffusion_map import diffusion_map
+import statistics as stat
 
 np.set_printoptions(precision=4, linewidth=200)
 
@@ -23,7 +24,6 @@ def compute_weight_norm(eig_mat):
 
 def cosine_similarity(a, b):
     return np.dot(a,b) / (npla.norm(a) * npla.norm(b))
-
 
 def Eros_PCA(A, B, weights):
     n = np.shape(A)[0]
@@ -93,6 +93,54 @@ def build_eig_mat_pca(folder):
 
     return eigs
 
+def compare_distances(folder1, folder2):
+    # Construct all the matrices
+    mat_1 = []
+    mat_2 = []
+
+    for file in os.listdir(folder1):
+        with open(os.path.join(folder2, file)) as f:
+            A = np.genfromtxt(f, delimiter=',', dtype=np.float64, skip_header=1)
+            A = np.array([[int(x.decode()) if isinstance(x, bytes) else x for x in row] for row in A]).T 
+            mat_1.append(A)
+
+    for file in os.listdir(folder2):
+        with open(os.path.join(folder2, file)) as f:
+            A = np.genfromtxt(f, delimiter=',', dtype=np.float64, skip_header=1)
+            A = np.array([[int(x.decode()) if isinstance(x, bytes) else x for x in row] for row in A]).T 
+            mat_2.append(A)
+
+    # Construct weights for interest class
+    eigs_mat_diff = build_eig_mat_diff(folder1)
+    eigs_mat_pca = build_eig_mat_pca(folder1)
+
+    weights_diff = compute_weight_norm(eig_mat_diff)
+    weights_pca = compute_weight_norm(eig_mat_pca)
+
+    difference_in_same_diff = []
+    difference_in_same_pca = []
+    for A_1 in mat_1:
+        for A_2 in mat_1:
+            if not np.all(A_1 == A_2):
+                difference_in_same_diff.append(Eros_Diffusion(A_1, A_2, weights_diff))
+                difference_in_same_pca.append(Eros_PCA(A_1,A_2, weights_pca))
+    
+    difference_in_diff_diff = []
+    difference_in_diff_pca = []
+
+    for A in mat_1:
+        for B in mat_2:
+            difference_in_diff_diff.append(Eros_Diffusion(A,B,weights_diff))
+            difference_in_diff_pca.append(Eros_PCA(A,B,weights_pca))
+
+    print('--- Diffusion Map ---')
+    print('Mean difference in same class:', stat.mean(difference_in_same_diff), ', Standard deviation:', stat.stdev(difference_in_same_diff))
+    print('Mean difference in different class:', stat.mean(difference_in_diff_diff), ', Standard deviation:', stat.stdev(difference_in_diff_diff))
+    print('-------- PCA --------')
+    print('Mean difference in same class:', stat.mean(difference_in_same_pca), ', Standard deviation:', stat.stdev(difference_in_same_pca))
+    print('Mean difference in different class:', stat.mean(difference_in_diff_pca), ', Standard deviation:', stat.stdev(difference_in_diff_pca))
+    print('---------------------')
+
 f = "Matrices/0x0a05956d2e3a21379af4abaa17bf883c04a67a7e.csv"
 A = np.genfromtxt(f, delimiter=',', dtype=np.float64, skip_header=1)
 A = np.array([[int(x.decode()) if isinstance(x, bytes) else x for x in row] for row in A]).T 
@@ -101,7 +149,7 @@ f = "Matrices/0xfdc27cb5c94095b2877ed9f688dd7a39d2bf45cd.csv"
 B = np.genfromtxt(f, delimiter=',', dtype=np.float64, skip_header=1)
 B = np.array([[int(x.decode()) if isinstance(x, bytes) else x for x in row] for row in B]).T 
 
-f = "transaction_data.csv"
+f = "Matrices/0xa611438e5637c227e5080477b7180fc3d1c76710f2aa5f6d71c28a1dee1e2ed4.csv"
 C = np.genfromtxt(f, delimiter=',', dtype=np.float64, skip_header=1)
 C = np.array([[int(x.decode()) if isinstance(x, bytes) else x for x in row] for row in C]).T 
 
